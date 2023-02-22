10b5bbad1849ce2eb3b16771efa2337a85b4ff02
JHONATANGARCIAXD <licethcifuentes@colegioguanenta.edu.co>
2023-02-22 09:13:34 -0500

    se agrega programa.py

---
+++ mayor_2enteros.py
@@ -0,0 +1,23 @@
+# programa para verificar cual de los dos numeros enteros es el mayor
+
+print("-----------------------------------------")
+print("----------MAYOR 2 ENTEROS----------------")
+print("-----------------------------------------")
+
+
+# input
+x = int (input("digite el valor de x: "))
+y = int (input("digite el valor de y: "))
+
+
+#processing 
+if x == y:
+    # ouput
+    print("los numeros son iguales...")
+else:
+    if x > y:
+       mayor = x
+    else:
+       mayor = y
+       #ouput
+    print("el mayor entre " + str(x) + " y " + str(y) + "es" + str(mayor))
